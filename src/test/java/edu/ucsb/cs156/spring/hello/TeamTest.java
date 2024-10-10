ddcf24d29a4c08d8a9c2000d9db17a5a59effb32
Piyush Jadhav <pjadhav@ucsb.edu>
2024-10-10 16:23:47 -0700

    pj - fixed coverage and mutations

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -8,10 +8,16 @@ import org.junit.jupiter.api.Test;
 public class TeamTest {
 
     Team team;
+    Team team2;
+    Team team3;
 
     @BeforeEach
     public void setup() {
         team = new Team("test-team");
+        team2 = new Team("test-team2");
+        team2.addMember("test");
+        team3 = new Team("test-team");
+        team3.addMember("test");
     }
 
     @Test
@@ -21,5 +27,25 @@ public class TeamTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_correct() {
+        assert (team.equals(team));
+        assert !(team.equals("hello"));
+        assert !(team.equals(team2));
+        assert !(team.equals(team3));
+    }
+
+    @Test
+    public void hashCode_returns_correct() {
+        int result = team2.hashCode();
+        int expectedResult = 641690875;
+
+        assertEquals(expectedResult, result);
+
+    }
 }
